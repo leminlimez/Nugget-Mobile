08b153f6db640c2ef81227961fada1f13912b67d
leminlimez <59540996+leminlimez@users.noreply.github.com>
2024-09-09 10:46:58 -0400

    Initial Commit

---
+++ Nugget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nugget
+//
+//  Created by lemin on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nugget/NuggetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuggetApp.swift
+//  Nugget
+//
+//  Created by lemin on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NuggetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
