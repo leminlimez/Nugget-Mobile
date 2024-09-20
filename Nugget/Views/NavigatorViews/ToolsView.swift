0ed9610e53cc2224a995de595a773c759a5b1dce
leminlimez <59540996+leminlimez@users.noreply.github.com>
2024-09-20 13:58:01 -0400

    add tool categories

--- Nugget/Views/NavigatorViews/ToolsView.swift
+++ Nugget/Views/NavigatorViews/ToolsView.swift
@@ -19,12 +19,23 @@ struct ToolsView: View {
         var minVersion: Version = Version(string: "1.0")
     }
     
-    @State var tools: [GeneralOption] = [
-        .init(page: .MobileGestalt, view: AnyView(GestaltView()), title: NSLocalizedString("Mobile Gestalt", comment: "Title of tool"), imageName: "platter.filled.top.and.arrow.up.iphone"),
-        .init(page: .FeatureFlags, view: AnyView(FeatureFlagsView()), title: NSLocalizedString("Feature Flags", comment: "Title of tool"), imageName: "flag", minVersion: Version(string: "18.0")),
-        .init(page: .StatusBar, view: AnyView(StatusBarView()), title: NSLocalizedString("Status Bar", comment: "Title of tool"), imageName: "wifi"),
-        .init(page: .SpringBoard, view: AnyView(SpringboardTweaksView()), title: NSLocalizedString("SpringBoard", comment: "Title of tool"), imageName: "app.badge"),
-        .init(page: .Internal, view: AnyView(InternalOptionsView()), title: NSLocalizedString("Internal Options", comment: "Title of tool"), imageName: "internaldrive")
+    struct ToolCategory: Identifiable {
+        var id = UUID()
+        var title: String
+        var pages: [GeneralOption]
+    }
+    
+    @State var tools: [ToolCategory] = [
+        .init(title: "Sparserestore Tweaks", pages: [
+            .init(page: .MobileGestalt, view: AnyView(GestaltView()), title: NSLocalizedString("Mobile Gestalt", comment: "Title of tool"), imageName: "platter.filled.top.and.arrow.up.iphone"),
+            .init(page: .FeatureFlags, view: AnyView(FeatureFlagsView()), title: NSLocalizedString("Feature Flags", comment: "Title of tool"), imageName: "flag", minVersion: Version(string: "18.0")),
+            .init(page: .SpringBoard, view: AnyView(SpringboardTweaksView()), title: NSLocalizedString("SpringBoard", comment: "Title of tool"), imageName: "app.badge"),
+            .init(page: .Internal, view: AnyView(InternalOptionsView()), title: NSLocalizedString("Internal Options", comment: "Title of tool"), imageName: "internaldrive")
+        ]),
+        .init(title: "Domain Restore Tweaks (requires Skip Setup)", pages: [
+            .init(page: .StatusBar, view: AnyView(StatusBarView()), title: NSLocalizedString("Status Bar", comment: "Title of tool"), imageName: "wifi")
+        ])
+        
     ]
     
     let userVersion = Version(string: UIDevice.current.systemVersion)
@@ -32,25 +43,31 @@ struct ToolsView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach($tools) { option in
-                    if option.minVersion.wrappedValue <= userVersion {
-                        NavigationLink(destination: option.view.wrappedValue) {
-                            HStack {
-                                Image(systemName: option.imageName.wrappedValue)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 24, height: 24)
-                                    .foregroundColor(.blue)
-                                Text(option.title.wrappedValue)
-                                    .padding(.horizontal, 8)
-                                if applyHandler.isTweakEnabled(option.page.wrappedValue) {
-                                    // show that it is enabled
-                                    Spacer()
-                                    Image(systemName: "checkmark.seal")
-                                        .foregroundStyle(Color(.green))
+                ForEach($tools) { category in
+                    Section {
+                        ForEach(category.pages) { option in
+                            if option.minVersion.wrappedValue <= userVersion {
+                                NavigationLink(destination: option.view.wrappedValue) {
+                                    HStack {
+                                        Image(systemName: option.imageName.wrappedValue)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 24, height: 24)
+                                            .foregroundColor(.blue)
+                                        Text(option.title.wrappedValue)
+                                            .padding(.horizontal, 8)
+                                        if applyHandler.isTweakEnabled(option.page.wrappedValue) {
+                                            // show that it is enabled
+                                            Spacer()
+                                            Image(systemName: "checkmark.seal")
+                                                .foregroundStyle(Color(.green))
+                                        }
+                                    }
                                 }
                             }
                         }
+                    } header: {
+                        Text(category.title.wrappedValue)
                     }
                 }
             }
