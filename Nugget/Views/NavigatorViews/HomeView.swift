0db38bf68cfc6ce26b5fc408ccc330f75f12b4e6
leminlimez <59540996+leminlimez@users.noreply.github.com>
2024-09-22 16:05:14 -0400

    add f1shy to credits

--- Nugget/Views/NavigatorViews/HomeView.swift
+++ Nugget/Views/NavigatorViews/HomeView.swift
@@ -142,7 +142,7 @@ struct HomeView: View {
                     LinkCell(imageName: "khanhduytran", url: "https://github.com/khanhduytran0/SparseBox", title: "khanhduytran0", contribution: "SparseBox", circle: true)
                     LinkCell(imageName: "jjtech", url: "https://github.com/JJTech0130/TrollRestore", title: "JJTech0130", contribution: "Sparserestore", circle: true)
                     LinkCell(imageName: "disfordottie", url: "https://x.com/disfordottie", title: "disfordottie", contribution: "Some Global Flag Features", circle: true)
-//                    LinkCell(imageName: "lrdsnow", url: "https://github.com/Lrdsnow/EUEnabler", title: "Lrdsnow", contribution: "EU Enabler", circle: true)
+                    LinkCell(imageName: "f1shy-dev", url: "https://gist.github.com/f1shy-dev/23b4a78dc283edd30ae2b2e6429129b5#file-eligibility-plist", title: "f1shy-dev", contribution: "AI Enabler", circle: true)
                     LinkCell(imageName: "app.gift", url: "https://sidestore.io/", title: "SideStore", contribution: "em_proxy and minimuxer", systemImage: true, circle: true)
                     LinkCell(imageName: "cable.connector", url: "https://libimobiledevice.org", title: "libimobiledevice", contribution: "Restore Library", systemImage: true, circle: true)
                 } header: {
